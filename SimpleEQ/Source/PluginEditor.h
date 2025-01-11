45de83ef8d075a3cf733eb6c09b7e6bf7765f09a
NathanielDiCicco <nad426@lehigh.edu>
2025-01-11 16:17:21 -0500

    Skeleton for Sliders

--- SimpleEQ/Source/PluginEditor.h
+++ SimpleEQ/Source/PluginEditor.h
@@ -11,10 +11,33 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-struct CustomRotarySlider : juce::Slider{
-    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox){
-        
+struct LookAndFeel : juce::LookAndFeel_V4{
+    void drawRotarySlider (juce::Graphics&, int x, int y, int width, int height,
+                           float sliderPosProportional, float rotaryStartAngle,
+                           float rotaryEndAngle, juce::Slider&) override {}
+};
+
+struct RotarySliderWithLabels : juce::Slider{
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox),
+    param(&rap),
+    suffix(unitSuffix)
+    {
+        setLookAndFeel(&lnf);
     }
+    
+    ~RotarySliderWithLabels(){
+        setLookAndFeel(nullptr);
+    }
+    
+    void paint(juce::Graphics& g) override { }
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHeight() const {return 14;}
+    juce::String getDisplayString() const;
+    private:
+    LookAndFeel lnf;
+    
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
 };
 
 struct ResponseCurveComponent: juce::Component, juce::AudioProcessorParameter::Listener, juce::Timer{
@@ -57,7 +80,7 @@ class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
     SimpleEQAudioProcessor& audioProcessor;
     
     
-    CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
+    RotarySliderWithLabels peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
     ResponseCurveComponent responseCurveComponent;
     
     using APVTS = juce::AudioProcessorValueTreeState;
@@ -66,6 +89,6 @@ class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
     Attachment peakFreqSliderAttachment, peakGainSliderAttachment, peakQualitySliderAttachment, lowCutFreqSliderAttachment, highCutFreqSliderAttachment, lowCutSlopeSliderAttachment, highCutSlopeSliderAttachment;
     
     std::vector<juce::Component*> getComps();
-        
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
